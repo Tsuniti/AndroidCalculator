ffac8930d57992262c3036ea3e3c39c754577759
Tsuniti <Tsuniti00@gmail.com>
2025-05-01 15:26:04 +0200

    create calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView numberText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        numberText = findViewById(R.id.textNumber);
+    }
+
+
+
+
+    public void numberListener(View view){
+        Button button = (Button) view;
+
+        String text = button.getText().toString();
+//        Object tag = button.getTag();
+//        int id = button.getId();
+        //if...
+
+        numberText.append(text);
+    }
+    public void operatorListener(View view){
+
+        String text = numberText.getText().toString();
+
+        if(text.endsWith("+")
+                || text.endsWith("-")
+                || text.endsWith("*")
+                || text.endsWith("/"))
+        {
+            numberText.setText(text.substring(0, text.length() - 1));
+        }
+        else if(text.contains("+")
+        || text.contains("-")
+        || text.contains("*")
+        || text.contains("/")) {
+
+            result();
+        }
+
+            Button button = (Button) view;
+
+            numberText.append(button.getText().toString());
+
+
+    }
+    public void equalsListener(View view){
+
+        String text = numberText.getText().toString();
+
+        if(text.endsWith("+")
+                || text.endsWith("-")
+                || text.endsWith("*")
+                || text.endsWith("/"))
+        {
+            return;
+        }
+
+        result();
+    }
+    public void clearListener(View view){
+        numberText.setText("");
+    }
+
+    public void result()
+    {
+
+        double result;
+        String resultStr;
+
+        char operator = 0;
+        int operatorIndex = -1;
+        String expression = numberText.getText().toString();
+
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+                operator = c;
+                operatorIndex = i;
+                break;
+            }
+        }
+
+        if (operatorIndex == -1) {
+            return;
+        }
+
+        double first = Double.parseDouble(expression.substring(0, operatorIndex));
+        double second = Double.parseDouble(expression.substring(operatorIndex + 1));
+
+
+        switch (operator) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case '*':
+                result = first * second;
+                break;
+            case '/':
+                if (second == 0) {
+                    return;
+                }
+                result = first / second;
+                break;
+            default: throw new IllegalStateException("Неизвестный оператор: " + operator);
+        }
+
+        if(result == (long) result)
+        {
+            resultStr = String.valueOf((long) result);
+        }
+        else
+        {
+            resultStr = String.valueOf(result);
+        }
+
+        numberText.setText(resultStr);
+    }
+}
